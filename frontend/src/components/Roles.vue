b69f60afe5fef10cca387a00566bfad166607edb
schlakob <38752017+schlakob@users.noreply.github.com>
2020-04-23 15:10:12 +0200

    WT5ON-28: Updated Structure of FE

---
+++ frontend/src/components/Roles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label for="roleNameInput">Name der Rolle:</label>
+    <input type="text" name="roleNameInput" data-inputname="roleName" :value="roleName" @keyup="updateInput"/>
+    <p></p>
+    <label for="minRoleAmountInput">Minimale Anzahl der Rolle:</label>
+    <input type="number" name="minRoleAmountInput" data-inputname="minRoleAmount" :value="minRoleAmount" @keyup="updateInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Roles',
+  props: [
+      "roleName",
+      "minRoleAmount"
+  ],
+  methods: {
+      updateInput(e) {
+          const value = e.target.value
+          const name= e.target.dataset.inputname
+          this.$emit(`update${name}`, value)
+      }
+  }
+}
+</script>
--- frontend/src/components/SessionStart.vue
+++ frontend/src/components/SessionStart.vue
@@ -3,56 +3,70 @@
     <h1>Groupify</h1>
 
     <p></p>
-    <label for="minGroupSizeInput">Minimale Gruppengröße: </label>
-    <input type="number" name="minGroupSizeInput" v-model="minGroupSize">
+    <label for="minGroupSizeInput">Minimale Gruppengröße:</label>
+    <input type="number" name="minGroupSizeInput" v-model="form.minGroupSize" />
     <p style="height: 10px"></p>
 
-    <label for="roleNameInput">Name der Rolle: </label>
-    <input type="text" name="roleNameInput" v-model="roleName">
-    <p></p>
-    <label for="minRoleAmountInput">Minimale Anzahl der Rolle: </label>
-    <input type="number" name="minRoleAmountInput" v-model="minRoleAmount">
-    
+    <Roles
+      v-for="(role, index) in form.roles"
+      :key="index"
+      :roleName.sync="role.name"
+      :minRoleAmount.sync="role.minAmount"
+      v-on:updateroleName="form.roles[index].name = $event"
+      v-on:updateminRoleAmount="form.roles[index].minAmount = $event"
+    ></Roles>
+    <input type="button" value="Rolle hinzufügen" @click="addRole()" />
+
     <p style="height: 10px"></p>
-    <input type="button" value="Raum erstellen" @click="generateToken()">
-    <input type="text" name="generatedToken" id="generatedToken" :value="generatedToken" disabled>
+    <input type="button" value="Raum erstellen" @click="generateToken()" />
+    <input
+      type="text"
+      name="generatedToken"
+      id="generatedToken"
+      :value="form.generatedToken"
+      disabled
+    />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
+import Roles from "../components/Roles";
 export default {
-  name: 'SessionStart',
-  props: {
-  },
-  data () {
+  name: "SessionStart",
+  props: {},
+  data() {
     return {
-      generatedToken: "",
-      minGroupSize: "",
-      roleName: "",
-      minRoleAmount: ""
-    }
+      form: {
+        generatedToken: "",
+        minGroupSize: "",
+        roles: [
+          {
+            name: "",
+            minAmount: ""
+          }
+        ]
+      }
+    };
   },
   methods: {
-    generateToken(){      
-      axios.post('http://localhost:3000/rooms', {
-          "minGroupSize": this.minGroupSize,
-          "roles": [
-              {
-                  "name": this.roleName,
-                  "minAmount": this.minRoleAmount
-              }
-          ]
-      })
-      .then(function (response) {
-        // this.generatedToken = response.data.token;
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-      alert();
+    async generateToken() {
+      try {
+        const response = await axios.post(
+          "http://localhost:3000/rooms",
+          this.form
+        );
+        this.form.generatedToken = response.data.token;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    addRole() {
+      this.form.roles.push({name: "",minAmount: ""});
     }
+  },
+  components: {
+    Roles
   }
-}
+};
 </script>
