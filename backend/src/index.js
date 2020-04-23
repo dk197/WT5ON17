7da3ef2a264344e91dc97eff90fb3b5cad356b02
dk197 <38752159+dk197@users.noreply.github.com>
2020-04-23 10:53:22 +0200

    allow CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,15 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+
+// allow CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use(roomRouter)
 
 app.listen(port, () => {
