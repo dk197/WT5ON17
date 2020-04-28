f787de3b80508a58b18e7f51ed96fa1304d415e5
dk197 <38752159+dk197@users.noreply.github.com>
2020-04-28 12:14:00 +0200

    WT5ON-88 Create Socket listener

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,41 @@
 const express = require('express')
+const cors = require('cors')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 require('./db/mongoose')
 require('dotenv').config()
 const roomRouter = require('./router/roomRouter')
 
-const app = express()
 const port = process.env.PORT
 
+const whitelist = ['http://localhost:8080'];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin))
+      return callback(null, true)
+
+      callback(new Error('Not allowed by CORS'));
+  }
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 
 // allow CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+// });
 
 app.use(roomRouter)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
+
+http.listen(port, () => {
     console.log(`Server is up running at port ${port}`);
 })
