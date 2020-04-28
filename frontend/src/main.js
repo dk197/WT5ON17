626940cc503e2db2ced503e6a73e3b6eab80a8c2
dk197 <38752159+dk197@users.noreply.github.com>
2020-04-28 12:15:20 +0200

    WT5ON-88 Add socket Integration for frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,25 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
 
+const options = {}
+
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: 'http://localhost:3000',
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    },
+    options: options
+}))
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
