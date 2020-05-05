2f392c823c1de9bcaf4509f00261c0d69129e763
dk197 <38752159+dk197@users.noreply.github.com>
2020-05-05 13:57:16 +0200

    WT5ON-44 delete users when room is closed

--- backend/src/controller/roomController.js
+++ backend/src/controller/roomController.js
@@ -1,4 +1,5 @@
 const Room = require('../models/roomModel')
+const User = require('../models/userModel')
 
 module.exports = {
     createRoom: async function (req, res) {
@@ -16,7 +17,6 @@ module.exports = {
     verifyToken: async function (req, res) {
         try {
             const room = await Room.findOne({ token: req.params.token })
-            console.log(room);
             res.send(room)
         } catch (e) {
             console.log(e);
@@ -26,9 +26,11 @@ module.exports = {
     deleteRoom: async function (req, res) {
         try {
             const room = await Room.deleteOne({_id: req.params.id})
+            
             if (!room) {
                 return res.status(404).send()
             }
+            await User.deleteMany({roomId: req.params.id})
             res.send()
         } catch (e) {
             res.status(500).send()
