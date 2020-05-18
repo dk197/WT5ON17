0398ce5b73bca6f16fdbf32b5fd5d9ee3e43961f
dk197 <38752159+dk197@users.noreply.github.com>
2020-05-18 09:35:02 +0200

    Add Form to participate as admin

--- frontend/src/views/CreateRoom.vue
+++ frontend/src/views/CreateRoom.vue
@@ -1,95 +1,169 @@
 <template>
-  <div class="content">
-    <h2 style="line-height:1.2" class="heading">Gruppen-Einstellungen:</h2>
-    <div id=field>
-      <input class="input input-p" placeholder="Gruppenanzahl" type="number" name="groupAmount" v-model="form.groupAmount"/>
-      <label for="groupAmount">Anzahl an generierten Gruppen:</label>
-    </div>
-    <div class="roleWrap">
-      <Roles
-        v-for="(role, index) in form.roles"
-        :key="index"
-        :roleName.sync="role.name"
-        :minRoleAmount.sync="role.minAmount"
-        v-on:updateroleName="form.roles[index].name = $event"
-        v-on:updateminRoleAmount="form.roles[index].minAmount = $event"
-        @remove="removeRole(index)"
-      ></Roles>
-      <div class="alignright">
-      <input class="button button-s" type="button" value="Rolle hinzufügen" @click="addRole()" />
-      </div>
-    </div>
+    <div class="content">
+        <h2 style="line-height:1.2" class="heading">Gruppen-Einstellungen:</h2>
+        <div id="field">
+            <input
+                class="input input-p"
+                placeholder="Gruppenanzahl"
+                type="number"
+                name="groupAmount"
+                v-model="roomForm.groupAmount"
+            />
+            <label for="groupAmount">Anzahl an generierten Gruppen:</label>
+        </div>
+        <div class="roleWrap">
+            <Roles
+                v-for="(role, index) in roomForm.roles"
+                :key="index"
+                :roleName.sync="role.name"
+                :minRoleAmount.sync="role.minAmount"
+                v-on:updateroleName="roomForm.roles[index].name = $event"
+                v-on:updateminRoleAmount="roomForm.roles[index].minAmount = $event"
+                @remove="removeRole(index)"
+            ></Roles>
+            <div class="alignright">
+                <input
+                    class="button button-s"
+                    type="button"
+                    value="Rolle hinzufügen"
+                    @click="addRole()"
+                />
+            </div>
+        </div>
 
-    <div class="alignright">
-    <input class="button button-p" type="button" value="Raum erstellen" @click="generateToken()"/>
-    <input
-      class="input input-s"
-      type="text"
-      name="generatedToken"
-      id="generatedToken"
-      :value="form.generatedToken"
-      disabled
-    />
+        <div v-if="showParticipantForm">
+            <div>
+                <input class="input input-p" type="text" v-model="userForm.username" placeholder="Name" />
+            </div>
+            <div class="selectable-role" v-for="(role, index) in roomForm.roles" :key="index">
+                <input class="input-radio" type="radio" v-model="userForm.role" :value="role.name" />
+                <label>{{ role.name }}</label>
+            </div>
+        </div>
+
+        <div class="alignright">
+            <input
+                class="button button-s"
+                type="button"
+                value="Selbst teilnehmen"
+                @click="toggleParticipantForm()"
+            />
+        </div>
+
+        <div class="alignright">
+            <input
+                class="button button-p"
+                type="button"
+                value="Raum erstellen"
+                @click="generateToken()"
+            />
+            <input
+                class="input input-s"
+                type="text"
+                name="generatedToken"
+                id="generatedToken"
+                :value="roomForm.generatedToken"
+                disabled
+            />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import axios from "axios";
 import Roles from "../components/Roles";
 export default {
-  name: "CreateRoom",
-  props: {},
-  data() {
-    return {
-      form: {
-        generatedToken: "",
-        groupAmount: "",
-        roles: []
-      }
-    };
-  },
-  methods: {
-    async generateToken() {
-      try {
-        const response = await axios.post(
-          "http://localhost:3000/rooms",
-          this.form
-        );
-        console.log(response);
-        this.form.generatedToken = response.data.roomToken;
-        this.$store.commit('setRoomToken', response.data.roomToken)
-        this.$store.commit('setRoom', response.data.createdRoom)
-        this.$store.commit('setAdmin')
-        localStorage.setItem('authToken', response.data.createdRoomOwner.authToken)
-        this.$store.commit('addUser', response.data.createdRoomOwner)
-        this.$socket.emit("createRoom", response.data.roomToken);
-        this.$router.push({ path: `/room` })
-      } catch (e) {
-        console.log(e);
-      }
+    name: "CreateRoom",
+    props: {},
+    data() {
+        return {
+            roomForm: {
+                generatedToken: "",
+                groupAmount: "",
+                roles: []
+            },
+            userForm: {
+                role: "",
+                username: ""
+            },
+            showParticipantForm: false
+        };
     },
-    addRole() {
-      this.form.roles.push({name: "",minAmount: ""});
+    methods: {
+        async generateToken() {
+            try {
+                const response = await axios.post(
+                    "http://localhost:3000/rooms",
+                    {
+                        room: this.roomForm,
+                        user: this.userForm
+                    }
+                );
+                console.log(response);
+                this.roomForm.generatedToken = response.data.roomToken;
+                this.$store.commit("setRoomToken", response.data.roomToken);
+                this.$store.commit("setRoom", response.data.createdRoom);
+                this.$store.commit("setAdmin");
+                localStorage.setItem(
+                    "authToken",
+                    response.data.createdRoomOwner.authToken
+                );
+                this.$store.commit("addUser", response.data.createdRoomOwner);
+                this.$socket.emit("createRoom", response.data.roomToken);
+                this.$router.push({ path: `/room` });
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        addRole() {
+            this.roomForm.roles.push({ name: "", minAmount: "" });
+        },
+        removeRole(index) {
+            this.roomForm.roles.splice(index, 1);
+        },
+        toggleParticipantForm() {
+            this.showParticipantForm = !this.showParticipantForm;
+        }
     },
-    removeRole(index){
-      this.form.roles.splice(index, 1);
-    }
-  },
-  components: {
-    Roles
-  },
-  sockets: {
+    components: {
+        Roles
+    },
+    sockets: {
         connect() {
-            console.log('connected');
+            console.log("connected");
         }
     }
 };
-
 </script>
 
 <style>
-  .roleWrap {
+.roleWrap {
     margin-top: 50px;
+}
+.selectable-role {
+    margin: 10px 5px 0px 0;
+}
+.input-radio {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    height: 20px;
+    width: 20px;
+    outline: none;
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+    margin: 0;
+    margin-right: 10px;
+    cursor: pointer;
+    border-radius: 20px;
+    border: 4px solid #094440;
+    background: var(--b, var(--background));
+    transition: background .3s, border-color .3s, box-shadow .2s;
   }
+.input-radio:checked {
+    background-color: white;
+}
+.input-radio:focus {
+    box-shadow: 0 0 0 var(--focus);
+}
 </style>
