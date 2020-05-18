e01173230dd6b71df288062a1ee13862003bac29
dk197 <38752159+dk197@users.noreply.github.com>
2020-05-18 11:35:16 +0200

    Create component and eventbus for error popup

---
+++ frontend/src/components/ErrorPopup.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="showPopup">
+        <p>{{ errorMessage }}</p>
+        <button @click="closePopup">Schließen</button>
+    </div>
+</template>
+
+<script>
+import EventBus from '../eventbus/eventbus'
+export default {
+    data() {
+        return {
+            showPopup: false,
+            errorMessage: ''
+        }
+    },
+    mounted() {
+        EventBus.$on('showErrorPopup', function (errorMessage) {
+            this.errorMessage = errorMessage
+            this.showPopup = true
+        });
+    },
+    methods: {
+        closePopup() {
+            this.showPopup = false
+        }
+    }
+}
+</script>
+
