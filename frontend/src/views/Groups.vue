af93e2e9d2af0a4b7cabf3224f1d2cf855be8836
schlakob <38752017+schlakob@users.noreply.github.com>
2020-05-19 15:45:34 +0200

    unused code

--- frontend/src/views/Groups.vue
+++ frontend/src/views/Groups.vue
@@ -2,18 +2,6 @@
     <div class="content">
         <h2>Gruppen:</h2>
         <Group v-for="(group, index) in getAllGroups" :group="group" :groupIndex="index" :key="index"></Group>
-        <!-- <div class="Progress">
-          <div class="Progress-content">
-              <div class="Bar">
-              </div>
-              <div class="step step1 done">1
-              </div>
-              <div class="step step2 active">2
-              </div>
-              <div class="step step3">3
-              </div>
-          </div>
-      </div> -->
     </div>
     
 </template>
