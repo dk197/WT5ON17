226d0a760a0ce58a72ff2a5a5610f071a25c3d5f
dk197 <38752159+dk197@users.noreply.github.com>
2020-05-20 12:23:50 +0200

    Delete old routes

--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -4,12 +4,7 @@ const roomController = require('../controller/roomController')
 const userController = require('../controller/userController')
 
 router.post('/rooms', roomController.createRoom)
-router.post('/rooms/phase', roomController.changePhase)
-
 router.get('/rooms/:token', roomController.verifyToken)
-
-router.delete('/rooms/:id', roomController.deleteRoom)
-
 router.post('/user', userController.createUser)
 
 module.exports = router
